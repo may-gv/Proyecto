48b023ca6fcd4706b44a4bd3760cb6ba7571c366
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-11-08 16:46:05 -0600

    Carrusel
    Agregamos un carrusel para que la vista del inicio sea mas estética y llamativa para el usuario

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,9 +2,30 @@
 
 
 @section('contenido')
+<br>
 
- <h1>Inicio Administrador</h1>
 
+ <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="css\images\descarga.png" class="d-block w-100" width="380" height="600" alt="...">
+      </div>
+
+      <div class="carousel-item">
+        <img src="css\images\Comic.jpg" class="d-block w-100" width="380" height="600" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  
+<br><br><br><br>
 
     <div class="row">
         <div class="col-4 ">
