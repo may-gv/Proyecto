ecc1904eaa272d69d89c6227c6d24ff9bbd9a8a1
may-gv <119034024@upq.edu.mx>
2022-11-09 01:11:01 -0600

    forma al carrusel

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,16 +3,22 @@
 
 @section('contenido')
 <br>
-
+<div class="container mt-5 col-md-4 " id="car">
 
  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="css\images\descarga.png" class="d-block w-100" width="380" height="600" alt="...">
+        <img src="https://as01.epimg.net/meristation/imagenes/2022/04/22/reportajes/1650661515_467770_1650696764_noticia_normal.jpg" class="d-block w-100" width="380" height="600" alt="...">
       </div>
 
       <div class="carousel-item">
-        <img src="css\images\Comic.jpg" class="d-block w-100" width="380" height="600" alt="...">
+        <img src="https://i.etsystatic.com/13702629/r/il/067798/1979047720/il_570xN.1979047720_kthx.jpg" class="d-block w-100" width="380" height="600" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://http2.mlstatic.com/D_NQ_NP_833761-MLA45474338711_042021-O.jpg" class="d-block w-100" width="380" height="600" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://www.clubdelectores.cl/wp-content/uploads/1661873759-61.jpg" class="d-block w-100" width="380" height="600" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -27,29 +33,8 @@
   
 <br>
 
-    <div class="row">
-        <div class="col-4 ">
-            <div class="card text-center mb-5 ml-5 fw-bold" style="width: 24rem; margin-left: 1rem">
-                
-        
-                <div class="card-header text-center">
-                    Administracion de Usuarios
-                </div>
-                <img src="css\images\descarga.png" class="card-img-top" alt="...">
-        
-                <div class="card-body">
-                    <p class="card-text"> Aqui se mandara al formulario y consulta para poder agregar, eliminar y editar usuarios del sistema. </p>
-        
-                </div>
-        
-        
-        
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-secondary" id="cuerpo"> Usuarios </button>
-                </div>
-        
-        
-            </div> 
+   
+      </div> 
 
 
 @stop
