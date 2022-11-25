3ef936a52c77e623d9d8490963736699d538da23
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-11-24 22:51:18 -0600

    Cambiamos el id

--- database/migrations/2022_11_25_044510_create_tb_proveedores.php
+++ database/migrations/2022_11_25_044510_create_tb_proveedores.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('tb_proveedores', function (Blueprint $table) {
-            $table->increments(id);
+            $table->increments('idProo');
             $table->string('Empresa');
             $table->string('Tipomercancia');
             $table->string('Direccion');
